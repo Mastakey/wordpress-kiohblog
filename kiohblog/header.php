abd4fca23a74d71e2ff75befce0cbe78f3412b55
Kioh Han <kioh.han@gmail.com>
2018-06-26 09:52:38 -0400

    Fix head title

--- kiohblog/header.php
+++ kiohblog/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?php echo bloginfo('name'); ?><?php wp_title( '|', true, 'left' ); ?></title>
+    <title><?php echo bloginfo('name'); ?> | <?php wp_title( '|', true, 'left' ); ?></title>
     <?php
         wp_head();
     ?>
